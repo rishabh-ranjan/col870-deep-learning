4983829543ea626ae1766e035afa64e2f4fa56cc
daman1209arora <45387992+daman1209arora@users.noreply.github.com>
2021-04-20 14:48:40 +0530

    updated file to handle crf

--- A1/LSTM-CRF/train_ner.py
+++ A1/LSTM-CRF/train_ner.py
@@ -3,6 +3,7 @@
 from ner import *    
 
 import torch
+import crf
 
 import argparse
 import logging
@@ -28,67 +29,67 @@ parser.add_argument('--stats_file')
 args = parser.parse_args()
 
 logging.info('loading glove embeddings')
-cpath = os.path.join(args.cache_dir, 'tok_to_id__glv_emb.pt')
-if os.path.isfile(cpath):
-    logging.info('\tusing cache')
-    tok_to_id, glv_emb = torch.load(cpath)
-else:
-    logging.info('\tcomputing fresh')
-    tok_to_id, glv_emb = load_emb(args.glove_embeddings_file, int(4e5))
-    if os.path.isdir(args.cache_dir):
-        logging.info('caching')
-        torch.save((tok_to_id, glv_emb), cpath)
+#cpath = os.path.join(args.cache_dir, 'tok_to_id__glv_emb.pt')
+#if os.path.isfile(cpath):
+#    logging.info('\tusing cache')
+#    tok_to_id, glv_emb = torch.load(cpath)
+#else:
+#    logging.info('\tcomputing fresh')
+tok_to_id, glv_emb = load_emb(args.glove_embeddings_file, int(4e5))
+    #if os.path.isdir(args.cache_dir):
+    #    logging.info('caching')
+    #    torch.save((tok_to_id, glv_emb), cpath)
 
 logging.info('loading character dictionary')
-cpath = os.path.join(args.cache_dir, 'chr_to_id.pt')
-if os.path.isfile(cpath):
-    logging.info('\tusing cache')
-    chr_to_id = torch.load(cpath)
-else:
-    logging.info('\tcomputing fresh')
-    chr_to_id = load_chrs(os.path.join(args.data_dir, 'train.txt'))
-    if os.path.isdir(args.cache_dir):
-        logging.info('caching')
-        torch.save(chr_to_id, cpath)
+#cpath = os.path.join(args.cache_dir, 'chr_to_id.pt')
+#if os.path.isfile(cpath):
+#    logging.info('\tusing cache')
+#    chr_to_id = torch.load(cpath)
+#else:
+#    logging.info('\tcomputing fresh')
+chr_to_id = load_chrs(os.path.join(args.data_dir, 'train.txt'))
+    #if os.path.isdir(args.cache_dir):
+        #logging.info('caching')
+        #torch.save(chr_to_id, cpath)
 
 logging.info('loading class labels')
-cpath = os.path.join(args.cache_dir, 'lbl_to_id__id_to_lbl.pt')
-if os.path.isfile(cpath):
-    logging.info('\tusing cache')
-    lbl_to_id, id_to_lbl = torch.load(cpath)
-else:
-    logging.info('\tcomputing fresh')
-    lbl_to_id, id_to_lbl = load_classes(os.path.join(args.data_dir, 'train.txt'))
-    if os.path.isdir(args.cache_dir):
-        logging.info('caching')
-        torch.save((lbl_to_id, id_to_lbl), cpath)
+#cpath = os.path.join(args.cache_dir, 'lbl_to_id__id_to_lbl.pt')
+#if os.path.isfile(cpath):
+    #logging.info('\tusing cache')
+    #lbl_to_id, id_to_lbl = torch.load(cpath)
+#else:
+    #logging.info('\tcomputing fresh')
+lbl_to_id, id_to_lbl = load_classes(os.path.join(args.data_dir, 'train.txt'))
+    #if os.path.isdir(args.cache_dir):
+        #logging.info('caching')
+        #torch.save((lbl_to_id, id_to_lbl), cpath)
 
 logging.info('writing token dictionary, character dictionary and class labels to vocabulary file')
 torch.save((tok_to_id, chr_to_id, lbl_to_id, id_to_lbl), args.vocabulary_output_file)
 
 logging.info('loading train set')
-cpath = os.path.join(args.cache_dir, 'train_W__train_X__train_Y.pt')
-if os.path.isfile(cpath):
-    logging.info('\tusing cache')
-    train_W, train_X, train_Y = torch.load(cpath)
-else:
-    logging.info('\tcomputing fresh')
-    train_W, train_X, train_Y = load_data(os.path.join(args.data_dir, 'train.txt'), tok_to_id, lbl_to_id, chr_to_id)
-    if os.path.isdir(args.cache_dir):
-        logging.info('caching')
-        torch.save((train_W, train_X, train_Y), cpath)
+#cpath = os.path.join(args.cache_dir, 'train_W__train_X__train_Y.pt')
+#if os.path.isfile(cpath):
+    #logging.info('\tusing cache')
+    #train_W, train_X, train_Y = torch.load(cpath)
+#else:
+    #logging.info('\tcomputing fresh')
+train_W, train_X, train_Y = load_data(os.path.join(args.data_dir, 'train.txt'), tok_to_id, lbl_to_id, chr_to_id)
+    #if os.path.isdir(args.cache_dir):
+        #logging.info('caching')
+        #torch.save((train_W, train_X, train_Y), cpath)
         
-logging.info('loading dev set')
-cpath = os.path.join(args.cache_dir, 'dev_W__dev_X__dev_Y.pt')
-if os.path.isfile(cpath):
-    logging.info('\tusing cache')
-    dev_W, dev_X, dev_Y = torch.load(cpath)
-else:
-    logging.info('\tcomputing fresh')
-    dev_W, dev_X, dev_Y = load_data(os.path.join(args.data_dir, 'dev.txt'), tok_to_id, lbl_to_id, chr_to_id)
-    if os.path.isdir(args.cache_dir):
-        logging.info('caching')
-        torch.save((dev_W, dev_X, dev_Y), cpath)
+#logging.info('loading dev set')
+#cpath = os.path.join(args.cache_dir, 'dev_W__dev_X__dev_Y.pt')
+#if os.path.isfile(cpath):
+    #logging.info('\tusing cache')
+    #dev_W, dev_X, dev_Y = torch.load(cpath)
+#else:
+    #logging.info('\tcomputing fresh')
+dev_W, dev_X, dev_Y = load_data(os.path.join(args.data_dir, 'dev.txt'), tok_to_id, lbl_to_id, chr_to_id)
+    #if os.path.isdir(args.cache_dir):
+        #logging.info('caching')
+        #torch.save((dev_W, dev_X, dev_Y), cpath)
 
 logging.info('setting device')
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -109,7 +110,7 @@ if args.initialization == 'random':
 elif args.initialization == 'glove':
     init_emb = glv_emb
 else:
-    assert(false)
+    assert(False)
     
 tok_emb_model = TokEmbModel(
     init_emb=init_emb,
@@ -133,57 +134,86 @@ elif args.char_embeddings == '1':
     )
     emb_size = 150
 else:
-    assert(false)
+    assert(False)
     
 if args.layer_normalization == '0':
     seq_tag_model = SeqTagModel(
         input_size=emb_size,
         hidden_size=100,
-        output_size=len(lbl_to_id)-1,
+        output_size=len(lbl_to_id) - 1 if args.crf == '0' else len(lbl_to_id) + 1,
         dropout_prob=0.5,
     )
 elif args.layer_normalization == '1':
     seq_tag_model = LNSeqTagModel(
         input_size=emb_size,
         hidden_size=100,
-        output_size=len(lbl_to_id)-1,
+        output_size=len(lbl_to_id) - 1 if args.crf == '0' else len(lbl_to_id) + 1,
         dropout_prob=0.5
     )
 else:
-    assert(false)
+    assert(False)
     
 # TODO: CRF
+if args.crf == '0':
+    ner_model = NERModel(
+        embed_model=embed_model,
+        seq_tag_model=seq_tag_model,
+        pad_lbl_id=lbl_to_id['PAD_LBL'],
+        pad_tok_id=tok_to_id['PAD_TOK']
+    )
 
-ner_model = NERModel(
-    embed_model=embed_model,
-    seq_tag_model=seq_tag_model,
-    pad_lbl_id=lbl_to_id['PAD_LBL'],
-    pad_tok_id=tok_to_id['PAD_TOK']
-)
+    logging.info('moving model to device')
+    ner_model = ner_model.to(device)
 
-logging.info('moving model to device')
-ner_model = ner_model.to(device)
+    logging.info('training')
 
-logging.info('training')
+    stats = train_loop(
+        train_set=(train_W, train_X, train_Y),
+        dev_set=(dev_W, dev_X, dev_Y),
+        model=ner_model,
+        lr=1e-3,
+        cos_max=100,
+        n_classes=len(lbl_to_id)-1,
+        train_batch_size=128,
+        dev_batch_size=128,
+        grad_clip_norm=5,
+        patience=5,
+        max_epochs=100,
+        show=False, 
+        id_to_lbl=id_to_lbl, 
+        pad_lbl_id=lbl_to_id['PAD_LBL']
+    )
 
-stats = train_loop(
-    train_set=(train_W, train_X, train_Y),
-    dev_set=(dev_W, dev_X, dev_Y),
-    model=ner_model,
-    lr=1e-3,
-    cos_max=100,
-    n_classes=len(lbl_to_id)-1,
-    train_batch_size=128,
-    dev_batch_size=128,
-    grad_clip_norm=5,
-    patience=5,
-    max_epochs=100,
-    show=False
-)
+    logging.info('saving model state dict')
+    torch.save(ner_model.state_dict(), args.output_file)
 
-logging.info('saving model state dict')
-torch.save(ner_model.state_dict(), args.output_file)
+    if args.stats_file:
+        logging.info('saving stats')
+        torch.save(stats, args.stats_file)
 
-if args.stats_file:
-    logging.info('saving stats')
-    torch.save(stats, args.stats_file)
+else:
+    logging.info('trying crf')
+    ner_model = NERModel(
+        embed_model=embed_model,
+        seq_tag_model=seq_tag_model,
+        pad_lbl_id=lbl_to_id['PAD_LBL'],
+        pad_tok_id=tok_to_id['PAD_TOK']
+    )
+
+    logging.info('moving model to device')
+    ner_model = ner_model.to(device)
+
+    logging.info('training')
+    id_to_lbl.append('START_LBL')
+    lbl_to_id['START_LBL'] = len(id_to_lbl) - 1
+    
+    print(id_to_lbl, lbl_to_id)
+    crf.train( train_set=(train_W.cpu(), train_X.cpu(), train_Y.cpu()),
+                dev_set=(dev_W.cpu(), dev_X.cpu(), dev_Y.cpu()),
+                ner_model=ner_model,
+                id_to_lbl=id_to_lbl, 
+                lbl_to_id=lbl_to_id,
+                pad_lbl_id=lbl_to_id['PAD_LBL'],
+                output_file=args.output_file,
+                
+             )
